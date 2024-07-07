a5e69fed4f5dbcfc7390bcdcd2c49705018409a7
Andreea Zegrean <149403601+AndreeaZegrean@users.noreply.github.com>
2024-07-07 10:39:23 +0300

    Update test_Update_Playlist.py

--- tests/test_Update_Playlist.py
+++ tests/test_Update_Playlist.py
@@ -1,19 +1,20 @@
-import pytest
+import unittest
+
+from environment import get_token
 from requests_folder.playlist.update_playlist import update_playlist
 
-class TestUpdatePlaylist:
+class TestUpdatePlaylist(unittest.TestCase):
+
+    def setUp(self):
+        self.token = get_token()
+
     def test_update_playlist(self):
         playlist_id = '1qISrmv0EN157fcAD0NOaE'
         new_name = 'Playlist Deea'
         new_description = 'This playlist has been updated using Spotify API'
         new_public_status = False
-
-        update_response = update_playlist(playlist_id, new_name, new_description, new_public_status)
-
+        update_response = update_playlist(self.token,playlist_id, new_name, new_description, new_public_status)
         assert update_response is not None, 'Failed to update playlist'
         assert update_response['name'] == new_name, 'Playlist name does not match'
         assert update_response['description'] == new_description, 'Playlist description does not match'
         assert update_response['public'] == new_public_status, 'Playlist visibility does not match'
-
-    if __name__ == "__main__":
-        pytest.main()
