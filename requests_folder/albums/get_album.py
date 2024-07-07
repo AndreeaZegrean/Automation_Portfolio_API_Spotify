8a57d347d88250776c512ef40c0f890903533850
Andreea Zegrean <149403601+AndreeaZegrean@users.noreply.github.com>
2024-07-07 10:40:00 +0300

    Update get_album.py

--- requests_folder/albums/get_album.py
+++ requests_folder/albums/get_album.py
@@ -1,22 +1,21 @@
 import requests
-from environment import token
 
-def get_album(album,market=''):
+def get_album(token,album,market=''):
     header = {'Authorization': token}
     response = requests.get(f'https://api.spotify.com/v1/albums/{album}?market={market}', headers=header)
     return response
 
-def get_album_without_market(id):
+def get_album_without_market(token,id):
     header = {'Authorization': token}
     response = requests.get(f'https://api.spotify.com/v1/albums/{id}', headers=header)
     return response
 
-def get_album_tracks(id):
+def get_album_tracks(token,id):
     header = {'Authorization': token}
     response = requests.get(f'https://api.spotify.com/v1/albums/{id}/tracks', headers=header)
     return response.json()
 
-def get_album_details(album_id):
+def get_album_details(token,album_id):
     headers = {'Authorization': token}
     response = requests.get(f'https://api.spotify.com/v1/albums/{album_id}', headers=headers)
     return response.json()
