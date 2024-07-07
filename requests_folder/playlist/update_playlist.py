7a5e1882b31e12e41dd673bce45e1022708488dc
Andreea Zegrean <149403601+AndreeaZegrean@users.noreply.github.com>
2024-07-07 10:49:23 +0300

    Update update_playlist.py

--- requests_folder/playlist/update_playlist.py
+++ requests_folder/playlist/update_playlist.py
@@ -1,15 +1,7 @@
 import requests
-from environment import token
 
-def update_playlist(playlist_id, new_name, new_description, new_public_status, header):
+def get_album_tracks(token,id):
     header = {'Authorization': token}
-    data = {
-        'name': new_name,
-        'description': new_description,
-        'public': new_public_status
-    }
-
-    response = requests.put(f'https://api.spotify.com/v1/playlists/{playlist_id}', headers=headers, json=data)
-    assert update_playlist.status_code == 200
-
+    response = requests.get(f'https://api.spotify.com/v1/albums/{id}/tracks', headers=header)
+    return response.json()
 
