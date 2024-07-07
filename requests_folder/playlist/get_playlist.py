e41db24672cf09db24f350429629550fc77385b1
Andreea Zegrean <149403601+AndreeaZegrean@users.noreply.github.com>
2024-07-07 10:48:53 +0300

    Update get_playlist.py

--- requests_folder/playlist/get_playlist.py
+++ requests_folder/playlist/get_playlist.py
@@ -1,9 +1,6 @@
 import requests
 
-from constants import USERNAME
-from environment import token
-
-def create_playlist(user_id, playlist_name, public=False, description=""):
+def create_playlist(token,user_id, playlist_name, public=False, description=""):
     header = {'Authorization': token}
     body = {
         'name': playlist_name,
@@ -11,15 +8,14 @@ def create_playlist(user_id, playlist_name, public=False, description=""):
         'public': public
     }
     response = requests.post(f'https://api.spotify.com/v1/users/{user_id}/playlists', json=body, headers=HEADER)
-    assert create_playlist.status_code == 200
     return response.json()
 
 def get_playlist(playlist,market=''):
     header = {'Authorization': token}
-    response = requests.get(f'https://api.spotify.com/v1/albums/{playlist}?market={market}', headers=header)
+    response = requests.get(f'https://api.spotify.com/v1/albums/{playlist}?market={market}', headers=HEADER)
     return response
 
 def get_playlist_details(playlist_id):
     header = {'Authorization': token}
-    response = requests.get(f'https://api.spotify.com/v1/playlists/{playlist_id}', headers=headers)
+    response = requests.get(f'https://api.spotify.com/v1/playlists/{playlist_id}', headers=HEADER)
     return response.json()
