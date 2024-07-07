2444d12d19d7c2157776eed391a6ae090e90b054
Andreea Zegrean <149403601+AndreeaZegrean@users.noreply.github.com>
2024-07-07 10:40:22 +0300

    Update get_album_tracks.py

--- requests_folder/albums/get_album_tracks.py
+++ requests_folder/albums/get_album_tracks.py
@@ -1,8 +1,9 @@
 import requests
-from environment import token
 
-def get_album_tracks(id):
+def get_album_tracks(token,id):
     header = {'Authorization': token}
     response = requests.get(f'https://api.spotify.com/v1/albums/{id}/tracks', headers=header)
     return response.json()
+
+
 
