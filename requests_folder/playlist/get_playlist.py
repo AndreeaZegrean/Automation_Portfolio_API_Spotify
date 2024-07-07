b63b9e472ebbd3dbcd8ccada855297dcdca63436
Andreea Zegrean <149403601+AndreeaZegrean@users.noreply.github.com>
2024-07-07 10:50:26 +0300

    Update get_playlist.py

--- requests_folder/playlist/get_playlist.py
+++ requests_folder/playlist/get_playlist.py
@@ -7,15 +7,15 @@ def create_playlist(token,user_id, playlist_name, public=False, description=""):
         'description': description,
         'public': public
     }
-    response = requests.post(f'https://api.spotify.com/v1/users/{user_id}/playlists', json=body, headers=HEADER)
+    response = requests.post(f'https://api.spotify.com/v1/users/{user_id}/playlists', json=body, headers=header)
     return response.json()
 
 def get_playlist(playlist,market=''):
     header = {'Authorization': token}
-    response = requests.get(f'https://api.spotify.com/v1/albums/{playlist}?market={market}', headers=HEADER)
+    response = requests.get(f'https://api.spotify.com/v1/albums/{playlist}?market={market}', headers=header)
     return response
 
 def get_playlist_details(playlist_id):
     header = {'Authorization': token}
-    response = requests.get(f'https://api.spotify.com/v1/playlists/{playlist_id}', headers=HEADER)
+    response = requests.get(f'https://api.spotify.com/v1/playlists/{playlist_id}', headers=header)
     return response.json()
