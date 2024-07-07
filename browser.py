84d17ab1af12f228a52273104d2a3622a736d3e1
Andreea Zegrean <149403601+AndreeaZegrean@users.noreply.github.com>
2024-07-07 10:35:30 +0300

    Update browser.py

--- browser.py
+++ browser.py
@@ -2,9 +2,10 @@ from seleniumbase import Driver
 
 class Browser():
 
-    driver = Driver()
-    driver.maximize_window()
-    driver.get('https://open.spotify.com/')
+    def __init__(self):
+        self.driver = Driver()
+        self.driver.maximize_window()
+        self.driver.get('https://open.spotify.com/')
 
     def closeBrowser(self):
         self.driver.quit()
